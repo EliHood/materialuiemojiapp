4b0e2f50b2a735b7eb40479ff3c882b649d04f5a
Eli <eli@Elis-MacBook-Pro.local>
2019-03-26 01:43:15 -0400

    Updates

---
+++ precache-manifest.f8c65e2545866312267d9090c3721bf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b01d4a54ae9169de5fa",
+    "url": "/materialuiemojiapp/static/css/main.fcc412b3.chunk.css"
+  },
+  {
+    "revision": "4b01d4a54ae9169de5fa",
+    "url": "/materialuiemojiapp/static/js/main.2d43bc57.chunk.js"
+  },
+  {
+    "revision": "3d5d2ab43c48fece0165",
+    "url": "/materialuiemojiapp/static/js/runtime~main.1dd4f353.js"
+  },
+  {
+    "revision": "4ae4fa97413aac895857",
+    "url": "/materialuiemojiapp/static/js/2.5b17b34e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/materialuiemojiapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "09fbc30cf451bc37ad4a695b1d60972a",
+    "url": "/materialuiemojiapp/index.html"
+  }
+];
